b95708eb98194e701e4befae1d10c4c1268cd581
Valerio Pappalardo <valerio.pappalardo@databricks.com>
2023-06-30 00:36:03 +0100

    remove unnecessary rules

--- vnet.tf
+++ vnet.tf
@@ -12,33 +12,6 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = azurerm_resource_group.dp_rg.name
   tags                = local.tags
 }
-resource "azurerm_network_security_rule" "dp_aad" {
-  name                        = "AllowAAD-dp"
-  priority                    = 200
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "VirtualNetwork"
-  destination_address_prefix  = "AzureActiveDirectory"
-  resource_group_name         = azurerm_resource_group.dp_rg.name
-  network_security_group_name = azurerm_network_security_group.this.name
-}
-
-resource "azurerm_network_security_rule" "dp_azfrontdoor" {
-  name                        = "AllowAzureFrontDoor-dp"
-  priority                    = 201
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "VirtualNetwork"
-  destination_address_prefix  = "AzureFrontDoor.Frontend"
-  resource_group_name         = azurerm_resource_group.dp_rg.name
-  network_security_group_name = azurerm_network_security_group.this.name
-}
 
 resource "azurerm_subnet" "public" {
   name                 = "${local.prefix}-public"
